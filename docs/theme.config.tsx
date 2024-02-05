608b552a381f24e4e25936669aa3cf2dc74bbacf
dnnsjsk <dnnsjsk@gmail.com>
2024-02-05 01:36:17 +0100

    fix og image

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -14,7 +14,7 @@ const config: DocsThemeConfig = {
   docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   head: function useHead() {
     const { title } = useConfig();
-    const socialCard = "https://glaze.dev/og.jpeg";
+    const socialCard = "https://glaze.dev/og.png";
     return (
       <>
         <link rel="preconnect" href="https://rsms.me/" />
