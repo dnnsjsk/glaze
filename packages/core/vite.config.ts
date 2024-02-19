bdfbfa918c7548cf1e25893d5af6b4282946d1c8
dnnsjsk <dnnsjsk@gmail.com>
2024-02-19 03:10:49 +0100

    update vite.config.ts

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig, ViteDevServer } from "vite";
 import dts from "vite-plugin-dts";
 import { resolve } from "path";
 
-// const removeConsolePlugin = () => {
-//   return {
-//     name: "remove-console", // name of the plugin
-//     renderChunk(code: string) {
-//       return code.replace(/console\.log\(.*\);?/g, "");
-//     },
-//   };
-// };
+const removeConsolePlugin = () => {
+  return {
+    name: "remove-console", // name of the plugin
+    renderChunk(code: string) {
+      return code.replace(/console\.log\(.*\);?/g, "");
+    },
+  };
+};
 
 const forceFullReloadPlugin = () => {
   return {
@@ -44,7 +44,7 @@ export default defineConfig({
     minify: "esbuild",
   },
   plugins: [
-    // removeConsolePlugin(),
+    removeConsolePlugin(),
     forceFullReloadPlugin(),
     dts({
       include: ["./src/index.ts"],
