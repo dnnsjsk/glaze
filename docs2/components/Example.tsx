3c44a7f573ea42b4159e9ba91719d274443b0512
dnnsjsk <dnnsjsk@gmail.com>
2025-01-11 15:40:44 +0100

    Push from PHPStorm

--- docs2/components/Example.tsx
+++ docs2/components/Example.tsx
@@ -6,4 +6,32 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-export const Example = ({ children }: { children: ReactNode }) => {};
+export const Example = ({ children }: { children: ReactNode }) => {
+  const ref = useRef(null);
+  const isString = typeof children === "string";
+  const content = isString ? { __html: children } : { __html: "" };
+
+  useEffect(() => {
+    glaze({
+      lib: { gsap: { core: gsap } },
+      element: ref.current,
+      breakpoints: {
+        sm: "(min-width: 640px)",
+        lg: "(min-width: 1024px)",
+      },
+      presets: {
+        helicopter: "from:rotate-2160|duration-5",
+      },
+    });
+  }, []);
+
+  return (
+    <div ref={ref} suppressHydrationWarning>
+      <div
+        suppressHydrationWarning
+        className="flex h-max w-full items-center justify-center overflow-hidden bg-fd-card p-12 md:rounded-xl"
+        dangerouslySetInnerHTML={content}
+      />
+    </div>
+  );
+};
